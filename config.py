3c4f4c607f0fa4c88c1e85436b4bbaab0fb77607
Edson Samuel Jr <samuedson@gmail.com>
2021-11-07 19:27:05 -0300

    Create initial models and migrate database

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ db_password = environ.get('DB_PASSWORD', default='notes')
 db_port = environ.get('DB_PORT', default='5432')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = f"postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
